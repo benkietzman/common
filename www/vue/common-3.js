ebd73e40192c579686fddc82dbae431fdd9fc273
Ben Kietzman <ben@kietzman.org>
2022-01-18 12:17:23 -0600

    Saved work.

--- www/vue/common-3.js
+++ www/vue/common-3.js
@@ -175,8 +175,12 @@ Vue.component('commonMessages',
     // {{{ close()
     close()
     {
-    }
+    },
     // }}}
+    messages()
+    {
+      return common.m_messages;
+    }
   },
   // }}}
   // {{{ props
@@ -186,7 +190,7 @@ Vue.component('commonMessages',
   template:
   `
     <div>
-      <div v-for="message in common.messages" :class="'alert alert-' + message.Class + ' alert-dismissible fade in'">
+      <div v-for="message in messages()" :class="'alert alert-' + message.Class + ' alert-dismissible fade in'">
         <button class="close" data-dismiss="alert" aria-label="close" v-on:click="close(message.Index)">&times;</button>
         <strong v-show="message.Title">{{message.Title}}<br></strong>
         {{message.Body}}

ec548320b0060e70740e5f89d020d9665618a3b9
Ben Kietzman <ben@kietzman.org>
2022-02-28 12:25:52 -0600

    Saved work.

--- www/auth/module/Windows.php
+++ www/auth/module/Windows.php
@@ -251,7 +251,15 @@ class bk_Windows
             $request['Function'] = 'login';
             $request['User'] = $strUserID;
             $request['Password'] = $strPassword;
-            $request['Domain'] = 'itservices.sbc.com';
+            if (($strJson = file_get_contents(dirname(__FILE__).'/../../../common_addons/www/auth/modules.json')) !== false)
+            {
+              $modules = json_decode($strJson, true);
+              if (isset($modules['windows']) && isset($modules['windows']['domain']) && $modules['windows']['domain'] != '')
+              {
+                $request['Domain'] = $modules['windows']['domain'];
+              }
+              unset($modules);
+            }
             $request['reqApp'] = 'Common Library';
             $in[] = json_encode($request);
             unset($request);

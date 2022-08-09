232a4ff1fdae95bebb2685cb1be5c119f315659a
Kietzman, Ben (bk6471) <bk6471@att.com>
2022-08-09 09:25:39 -0400

    Saved work.

--- Utility.cpp
+++ Utility.cpp
@@ -473,7 +473,7 @@ extern "C++"
     // {{{ sslAccept()
     SSL *Utility::sslAccept(SSL_CTX *ctx, int fdSocket, string &strError)
     {
-      bool bRetry;
+      bool bRetry = false;
 
       return sslAccept(ctx, fdSocket, bRetry, strError);
     }
@@ -483,23 +483,26 @@ extern "C++"
       int nReturn;
       SSL *ssl = NULL;
 
-      bRetry = false; 
       ERR_clear_error();
-      if ((ssl = SSL_new(ctx)) == NULL)
+      if (!bRetry && (ssl = SSL_new(ctx)) == NULL)
       {
         strError = (string)"SSL_new() " + sslstrerror();
       }
-      else if (!SSL_set_fd(ssl, fdSocket))
+      else if (!bRetry && !SSL_set_fd(ssl, fdSocket))
       {
         strError = (string)"SSL_set_fd() " + sslstrerror();
       }
-      else if ((nReturn = SSL_accept(ssl)) <= 0)
-      {
-        strError = (string)"SSL_accept() " + sslstrerror(ssl, nReturn, bRetry);
-      }
       else
       {
-        bGood = true;
+        bRetry = false; 
+        if ((nReturn = SSL_accept(ssl)) <= 0)
+        {
+          strError = (string)"SSL_accept() " + sslstrerror(ssl, nReturn, bRetry);
+        }
+        else
+        {
+          bGood = true;
+        }
       }
       if (!bRetry && !bGood && ssl != NULL)
       {
@@ -514,7 +517,7 @@ extern "C++"
     // {{{ sslConnect()
     SSL *Utility::sslConnect(SSL_CTX *ctx, int fdSocket, string &strError)
     {
-      bool bRetry;
+      bool bRetry = false;
 
       return sslConnect(ctx, fdSocket, bRetry, strError);
     }
@@ -524,23 +527,26 @@ extern "C++"
       int nReturn;
       SSL *ssl = NULL;
 
-      bRetry = false; 
       ERR_clear_error();
-      if ((ssl = SSL_new(ctx)) == NULL)
+      if (!bRetry && (ssl = SSL_new(ctx)) == NULL)
       {
         strError = (string)"SSL_new() " + sslstrerror();
       }
-      else if (!SSL_set_fd(ssl, fdSocket))
+      else if (!bRetry && !SSL_set_fd(ssl, fdSocket))
       {
         strError = (string)"SSL_set_fd() " + sslstrerror();
       }
-      else if ((nReturn = SSL_connect(ssl)) != 1)
-      {
-        strError = (string)"SSL_connect() " + sslstrerror(ssl, nReturn, bRetry);
-      }
       else
       {
-        bGood = true;
+        bRetry = false; 
+        if ((nReturn = SSL_connect(ssl)) != 1)
+        {
+          strError = (string)"SSL_connect() " + sslstrerror(ssl, nReturn, bRetry);
+        }
+        else
+        {
+          bGood = true;
+        }
       }
       if (!bRetry && !bGood && ssl != NULL)
       {

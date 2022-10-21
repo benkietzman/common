7daf1e30d8bfc04c44b32b4976163635eec906cc
Ben Kietzman <ben@kietzman.org>
2022-10-20 19:30:07 -0500

    Saved work.

--- www/vue/common-5.js
+++ www/vue/common-5.js
@@ -117,7 +117,7 @@ Vue.component('commonMenu',
   template:
   `
     <div style="margin-bottom: 50px;">
-      <div v-if="common.menu" class="navbar navbar-expand-lg navbar-dark bg-secondary" role="navigation">
+      <nav v-if="common.menu" class="navbar navbar-expand-lg navbar-dark bg-secondary bg-gradient">
         <div class="container-fluid">
           <a v-if="common.application" class="navbar-brand" href="#/">{{common.application}}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigationbar" aria-controls="navigationbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -127,26 +127,28 @@ Vue.component('commonMenu',
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item" v-for="item in common.menu.left"><router-link class="nav-link" :class="item.active" :to="item.href">{{item.value}}</router-link></li>
             </ul>
-            <ul class="navbar-nav navbar-right me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav navbar-right">
               <li class="nav-item" v-for="item in common.menu.right"><router-link class="nav-link" :class="item.active" :to="item.href">{{item.value}}</router-link></li>
             </ul>
           </div>
         </div>
-      </div>
-      <div v-if="common.submenu" class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#subnavigationbar" aria-controls="subnavigationbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="subnavigationbar">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item" v-for="item in common.submenu.left"><router-link class="nav-link" :class="item.active" :to="item.href">{{item.value}}</router-link></li>
-            </ul>
-            <ul class="navbar-nav navbar-right me-auto mb-2 mb-lg-0">
-              <li class="nav-item" v-for="item in common.submenu.right"><router-link class="nav-link" :class="item.active" :to="item.href">{{item.value}}</router-link></li>
-            </ul>
+      </nav>
+      <div class="container">
+        <nav v-if="common.submenu" class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#subnavigationbar" aria-controls="subnavigationbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="subnavigationbar">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item" v-for="item in common.submenu.left"><router-link class="nav-link" :class="item.active" :to="item.href">{{item.value}}</router-link></li>
+              </ul>
+              <ul class="navbar-nav navbar-right">
+                <li class="nav-item" v-for="item in common.submenu.right"><router-link class="nav-link" :class="item.active" :to="item.href">{{item.value}}</router-link></li>
+              </ul>
+            </div>
           </div>
-        </div>
+        </nav>
       </div>
     </div>
   `

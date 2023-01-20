b646fa44e5b8012142dd29cb29b3a49f9e8586b7
Ben Kietzman <ben@kietzman.org>
2023-01-20 10:30:14 -0600

    Added radialStatus.js to Common framework.

---
+++ www/js/component/radialStatus.js
@@ -0,0 +1,60 @@
+// vim600: fdm=marker
+///////////////////////////////////////////
+// author     : Ben Kietzman
+// begin      : 2023-01-20
+// copyright  : kietzman.org
+// email      : ben@kietzman.org
+///////////////////////////////////////////
+export default
+{
+  // {{{ controller()
+  controller()
+  {
+    let c = common;
+    let last = 0;
+    let s = c.store('radialStatus',
+    {
+      c: c,
+      expire: () =>
+      {
+        let current = Date.now();
+        if ((current - last) >= 5000)
+        {
+          s.stat = null;
+          c.render('radialStatus', c.autoLoads['radialStatus']);
+        }
+      }
+    });
+    c.attachEvent('radialPurpose_status', (stat) =>
+    {
+      last = Date.now();
+      if (stat.detail.Activity)
+      {
+        stat.detail.Activity = stat.detail.Activity.split(',').join(' --> ');
+      }
+      s.stat = stat.detail;
+      c.render('radialStatus', c.autoLoads['radialStatus']);
+    });
+    setInterval(() => {s.expire()}, 250);
+  },
+  // }}}
+  // {{{ template
+  template: `
+    {{#if stat}}
+    <div style="background: black; border-radius: 10px; bottom: 4px; color: orange; display: inline-block; opacity: 0.8; left: 4px; padding: 8px 16px 12px 16px; position: fixed;">
+      {{stat.radialFunction}}
+      --&gt;
+      {{stat.radialInterface}}
+      {{#if stat.Function}}
+        --&gt;
+        {{stat.Function}}
+      {{/if}}
+      {{#if stat.Activity}}
+        --&gt;
+        {{stat.Activity}}
+      {{/if}}
+    </div>
+    {{/if}}
+  `
+  // }}}
+}

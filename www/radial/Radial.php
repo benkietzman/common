c297f1b6acd1209af773f29c76cdaaf810b01c75
Ben Kietzman <ben@kietzman.org>
2023-04-11 11:03:11 -0500

    Saved work.

--- www/radial/Radial.php
+++ www/radial/Radial.php
@@ -242,7 +242,7 @@ class Radial
           $unPick = 0;
         }
         $strServer = $servers[$unPick];
-        if ($handle = stream_socket_client('tls://'.$strServer.':'.$port, $nErrorNo, $strError, 10, STREAM_CLIENT_CONNECT, $this->m_streamContext))
+        if (($handle = stream_socket_client('tls://'.$strServer.':'.$port, $nErrorNo, $strError, 10, STREAM_CLIENT_CONNECT, $this->m_streamContext)) !== false)
         { 
           $bConnected = true;
         }

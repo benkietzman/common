0a76d12eea9d79459dafedd68eedea69a8a886cc
Ben Kietzman <ben@kietzman.org>
2023-05-02 08:21:42 -0500

    Saved work.

--- Central.cpp
+++ Central.cpp
@@ -366,6 +366,11 @@ extern "C++"
     void Central::getProcessStatus(time_t &CTime, float &fCpu, float &fMem, unsigned long &ulImage, unsigned long &ulResident)
     {
       pid_t nPid = getpid();
+
+      getProcessStatus(nPid, CTime, fCpu, fMem, ulImage, ulResident);
+    }
+    void Central::getProcessStatus(const pid_t nPid, time_t &CTime, float &fCpu, float &fMem, unsigned long &ulImage, unsigned long &ulResident)
+    {
       stringstream ssFile, ssStatus;
       ifstream inProc;
 

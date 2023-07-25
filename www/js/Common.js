fb3f87a3287bfa7e6cdd923e66cc9fdb7d52418c
Ben Kietzman <ben@kietzman.org>
2023-07-25 12:42:43 -0500

    Saved work.

--- www/js/Common.js
+++ www/js/Common.js
@@ -609,6 +609,28 @@ class Common
     this.m_bJwtInclusion = bEnable;
   }
   // }}}
+  // {{{ esc()
+  esc(v)
+  {
+    return v.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, (char) =>
+    {
+      switch (char)
+      {
+        case "\0": return "\\0";
+        case "\x08": return "\\b";
+        case "\x09": return "\\t";
+        case "\x1a": return "\\z";
+        case "\n": return "\\n";
+        case "\r": return "\\r";
+        case "\"":
+        case "'":
+        case "\\":
+        case "%": return "\\"+char;
+        default: return char;
+      }
+    });
+  }
+  // }}}
   // {{{ existStore()
   existStore(controller)
   {

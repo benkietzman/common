038053fc3d4b3ff7282fc772b6d469e88d507e0a
Ben Kietzman <ben@kietzman.org>
2023-08-07 13:49:56 -0500

    Saved work.

--- www/js/Common.js
+++ www/js/Common.js
@@ -43,7 +43,7 @@ class Common
         {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({'Application': strApplication})
+        body: JSON.stringify({'Application': this.application})
       });
     }
     if (this.isDefined(options.id))

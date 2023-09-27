891717c141815de448dfc28cd096e75fee20a8b2
Ben Kietzman <ben@kietzman.org>
2023-09-27 11:51:48 -0500

    Saved work.

--- www/js/component/menu.js
+++ www/js/component/menu.js
@@ -29,12 +29,12 @@ export default
           <div class="collapse navbar-collapse" id="navigationbar">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               {{#each c.menu.left}}
-              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}">{{#if icon}}<i class="bi bi-{{icon}}" style="margin-right: 2px;"></i>{{/if}}{{value}}</a></li>
+              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}">{{#if icon}}<i class="bi bi-{{icon}}" style="margin-right: 4px;"></i>{{/if}}{{value}}</a></li>
               {{/each}}
             </ul>
             <ul class="navbar-nav navbar-right">
               {{#each c.menu.right}}
-              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}">{{#if icon}}<i class="bi bi-{{icon}}" style="margin-right: 2px;"></i>{{/if}}{{value}}</a></li>
+              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}">{{#if icon}}<i class="bi bi-{{icon}}" style="margin-right: 4px;"></i>{{/if}}{{value}}</a></li>
               {{/each}}
             </ul>
           </div>
@@ -48,12 +48,12 @@ export default
           <div class="collapse navbar-collapse" id="subnavigationbar">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               {{#each c.submenu.left}}
-              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}"><i class="bi bi-{{icon}}" style="margin-right: 2px;"></i>{{value}}</a></li>
+              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}"><i class="bi bi-{{icon}}" style="margin-right: 4px;"></i>{{value}}</a></li>
               {{/each}}
             </ul>
             <ul class="navbar-nav navbar-right">
               {{#each c.submenu.right}}
-              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}"><i class="bi bi-{{icon}}" style="margin-right: 2px;"></i>{{value}}</a></li>
+              <li class="nav-item"><a class="nav-link {{active}}" href="#{{href}}"><i class="bi bi-{{icon}}" style="margin-right: 4px;"></i>{{value}}</a></li>
               {{/each}}
             </ul>
           </div>

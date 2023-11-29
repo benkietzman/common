e1dca33d35ac9b2ffd38ced55fd4fb18a5792e76
Ben Kietzman <ben@kietzman.org>
2023-11-29 13:48:34 -0600

    Saved work.

--- www/js/Common.js
+++ www/js/Common.js
@@ -118,9 +118,9 @@ class Common
       {
         return (v1 + v2);
       });
-      Handlebars.registerHelper('date', (unTimestamp) =>
+      Handlebars.registerHelper('date', (unTimestamp, strFormat) =>
       {
-        return new Date(unTimestamp * 1000).toString();
+        return (new Date(unTimestamp * 1000)).format(strFormat);
       });
       Handlebars.registerHelper('divide', (v1, v2) =>
       {

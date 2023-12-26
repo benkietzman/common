18393ab6fe747f3ecaf05e889e98ce86d46635db
Ben Kietzman <ben@kietzman.org>
2023-12-26 10:49:37 -0600

    Saved work.

--- Radial.cpp
+++ Radial.cpp
@@ -1081,34 +1081,6 @@ void Radial::setTimeout(const string strTimeout)
 // }}}
 // }}}
 // {{{ ssh
-// {{{ sshCommand()
-bool Radial::sshCommand(string &strSession, const string strCommand, string &strData, string &strError)
-{
-  bool bResult = false;
-  Json *ptRequest = new Json, *ptResponse = new Json;
-
-  ptRequest->i("Interface", "ssh");
-  ptRequest->i("Function", "command");
-  ptRequest->i("Session", strSession);
-  ptRequest->i("Command", strCommand);
-  if (request(ptRequest, ptResponse, strError))
-  {
-    bResult = true;
-  }
-  if (ptResponse->m.find("Session") == ptResponse->m.end() || ptResponse->m["Session"]->v.empty())
-  {
-    strSession.clear();
-  }
-  if (ptResponse->m.find("Response") != ptResponse->m.end() && !ptResponse->m["Response"]->v.empty())
-  {
-    strData = ptResponse->m["Response"]->v;
-  }
-  delete ptRequest;
-  delete ptResponse;
-
-  return bResult;
-}
-// }}}
 // {{{ sshConnect()
 bool Radial::sshConnect(const string strServer, const string strPort, const string strUser, const string strPassword, string &strSession, string &strData, string &strError)
 {
@@ -1155,6 +1127,34 @@ bool Radial::sshDisconnect(string &strSession, string &strError)
   if (ptResponse->m.find("Session") == ptResponse->m.end() || ptResponse->m["Session"]->v.empty())
   {
     strSession.clear();
+  }
+  delete ptRequest;
+  delete ptResponse;
+
+  return bResult;
+}
+// }}}
+// {{{ sshSend()
+bool Radial::sshSend(string &strSession, const string strCommand, string &strData, string &strError)
+{
+  bool bResult = false;
+  Json *ptRequest = new Json, *ptResponse = new Json;
+
+  ptRequest->i("Interface", "ssh");
+  ptRequest->i("Function", "send");
+  ptRequest->i("Session", strSession);
+  ptRequest->i("Request", strCommand);
+  if (request(ptRequest, ptResponse, strError))
+  {
+    bResult = true;
+  }
+  if (ptResponse->m.find("Session") == ptResponse->m.end() || ptResponse->m["Session"]->v.empty())
+  {
+    strSession.clear();
+  }
+  if (ptResponse->m.find("Response") != ptResponse->m.end() && !ptResponse->m["Response"]->v.empty())
+  {
+    strData = ptResponse->m["Response"]->v;
   }
   delete ptRequest;
   delete ptResponse;

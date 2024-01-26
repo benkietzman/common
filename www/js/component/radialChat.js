2f2cf44668e1ae59297845f4a3c3a42b39810b28
Ben Kietzman <ben@kietzman.org>
2024-01-26 14:38:44 -0600

    Saved work.

--- www/js/component/radialChat.js
+++ www/js/component/radialChat.js
@@ -65,9 +65,21 @@ export default
                 {
                   if (c.isDefined(data.User))
                   {
-                    s.users.push(data.User);
+                    let bFound = false;
+                    for (let i = 0; !bFound && i < s.users.length; i++)
+                    {
+                      if (s.users[i] == data.User)
+                      {
+                        bFound = true;
+                      }
+                    }
+                    if (!bFound)
+                    {
+                      s.users.push(data.User);
+                    }
                   }
                 }
+                s.users.sort();
                 s.u();
               }
             });
@@ -91,7 +103,7 @@ export default
   // {{{ template
   template: `
     <div style="position: relative; z-index: 1000;">
-      <div id="radial-slide-panel" class="bg-success" style="position: fixed; top: 140px; right: 0px;">
+      <div id="radial-slide-panel" class="bg-success" style="position: fixed; top: 160px; right: 0px;">
         <button id="radial-slide-opener" class="btn btn-sm btn-warning float-start" c-click="slide()" style="width: 33px; height: 33px; font-size: 18px; font-weight: bold; margin: 0px 0px 0px -33px; border-radius: 10px 0px 0px 10px; vertical-align: top;">&#8803;</button>
         {{#if menu}}
         <div id="radial-slide-content" style="padding: 10px;">

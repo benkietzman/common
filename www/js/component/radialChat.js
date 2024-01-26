4cb22be68027b2f7ea1ff170d9edb27a6cc4051d
Ben Kietzman <ben@kietzman.org>
2024-01-26 14:40:39 -0600

    Saved work.

--- www/js/component/radialChat.js
+++ www/js/component/radialChat.js
@@ -80,6 +80,10 @@ export default
                   }
                 }
                 s.users.sort();
+                if (s.user.length > 0)
+                {
+                  s.user = s.users[0];
+                }
                 s.u();
               }
             });
@@ -103,8 +107,8 @@ export default
   // {{{ template
   template: `
     <div style="position: relative; z-index: 1000;">
-      <div id="radial-slide-panel" class="bg-success" style="position: fixed; top: 160px; right: 0px;">
-        <button id="radial-slide-opener" class="btn btn-sm btn-warning float-start" c-click="slide()" style="width: 33px; height: 33px; font-size: 18px; font-weight: bold; margin: 0px 0px 0px -33px; border-radius: 10px 0px 0px 10px; vertical-align: top;">&#8803;</button>
+      <div id="radial-slide-panel" class="bg-warning" style="position: fixed; top: 160px; right: 0px;">
+        <button id="radial-slide-opener" class="btn btn-sm btn-warning float-start" c-click="slide()" style="width: 33px; height: 33px; font-size: 18px; font-weight: bold; margin: 0px 0px 0px -33px; border-radius: 10px 0px 0px 10px; vertical-align: top;"><i class="bi bi-chat"></i></button>
         {{#if menu}}
         <div id="radial-slide-content" style="padding: 10px;">
           <select class="form-control form-control-sm" c-model="user">

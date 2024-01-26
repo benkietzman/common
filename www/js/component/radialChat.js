daf961be870896d463d7c674188f2cd76646facb
Ben Kietzman <ben@kietzman.org>
2024-01-26 15:50:13 -0600

    Saved work.

--- www/js/component/radialChat.js
+++ www/js/component/radialChat.js
@@ -144,7 +144,6 @@ export default
   // }}}
   // {{{ template
   template: `
-    {{#isValid}}
     <div style="position: relative; z-index: 1000;">
       <div id="radial-slide-panel" class="bg-info" style="position: fixed; top: 160px; right: 0px;">
         <button id="radial-slide-opener" class="btn btn-sm btn-info float-start" c-click="slide()" style="width: 33px; height: 33px; font-size: 18px; font-weight: bold; margin: 0px 0px 0px -33px; border-radius: 10px 0px 0px 10px; vertical-align: top;"><i class="bi bi-chat-fill"></i></button>
@@ -170,7 +169,6 @@ export default
         {{/if}}
       </div>
     </div>
-    {{/isValid}}
   `
   // }}}
 }

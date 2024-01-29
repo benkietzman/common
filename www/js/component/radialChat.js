cf1795b8c7ed95ea60dd48e76511a83997d4366f
Ben Kietzman <ben@kietzman.org>
2024-01-29 12:10:10 -0600

    Saved work.

--- www/js/component/radialChat.js
+++ www/js/component/radialChat.js
@@ -33,8 +33,8 @@ export default
     {
       if (c.isValid() && s.user.v)
       {
-        s.history.push({Message: s.message.v, User: c.getUserID(), FirstName: c.getFirstName(), LastName: c.getLastName()});
-        let request = {Interface: 'live', 'Function': 'message', Request: {User: s.user.v, Message: {Action: 'chat', Message: s.message.v, User: c.getUserID()}, FirstName: c.getFirstName(), LastName: c.getLastName()} };
+        s.history.push({Message: s.message.v, User: c.getUserID(), FirstName: c.getUserFirstName(), LastName: c.getUserLastName()});
+        let request = {Interface: 'live', 'Function': 'message', Request: {User: s.user.v, Message: {Action: 'chat', Message: s.message.v, User: c.getUserID()}, FirstName: c.getUserFirstName(), LastName: c.getUserLastName()} };
         s.message.v = null;
         c.wsRequest(c.m_strAuthProtocol, request).then((response) => {});
         s.u();

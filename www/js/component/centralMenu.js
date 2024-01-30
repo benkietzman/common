0a7a5e9e483b95e5578f6946b678b453b7c5c03f
Ben Kietzman <ben@kietzman.org>
2024-01-30 12:29:12 -0600

    Saved work.

--- www/js/component/centralMenu.js
+++ www/js/component/centralMenu.js
@@ -67,7 +67,7 @@ export default
   template: `
     <div style="position: relative; z-index: 1000;">
       <div id="central-slide-panel" class="bg-success" style="position: fixed; top: 120px; right: 0px;">
-        <button id="central-slide-opener" class="btn btn-sm btn-success float-start" c-click="slideMenu()" style="width: 33px; height: 33px; font-size: 18px; font-weight: bold; margin: 0px 0px 0px -33px; border-radius: 10px 0px 0px 10px; vertical-align: top;" title="applications"><i class="bi bi-window"></i></button>
+        <button id="central-slide-opener" class="btn btn-sm btn-success float-start" c-click="slideMenu()" style="width: 33px; height: 33px; font-size: 18px; font-weight: bold; margin: 0px 0px 0px -33px; border-radius: 10px 0px 0px 10px; vertical-align: top;" title="applications"><i class="bi bi-app"></i></button>
         {{#if c.centralMenu.show}}
         <div id="central-slide-content" style="padding: 10px;">
           <select class="form-control form-control-sm" c-change="go()" c-model="application" c-json>

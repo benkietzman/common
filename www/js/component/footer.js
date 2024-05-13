d5bd123b08656f4b9c8ef62bd1988763d5c909ec
Ben Kietzman <ben@kietzman.org>
2024-05-13 08:28:06 -0500

    Saved work.

--- www/js/component/footer.js
+++ www/js/component/footer.js
@@ -44,19 +44,31 @@ export default
     <br>
     <div class="container">
       <div class="row">
-        <div class="col-md-4 text-muted credit">
-          {{#if c.footer.email}}<a href="mailto:{{c.footer.email}}?subject={{c.footer.subject}}">Contact Admin</a>{{/if}}
-          {{#if c.footer.email}}{{#if c.footer.subject}}{{#if c.footer.version}}---{{/if}}{{/if}}{{/if}}
-          {{c.footer.version}}
+        <div class="col-md-4 text-muted credit" style="font-size:10px;">
+          {{#if c.footer.year}}&copy; {{c.footer.year}}{{/if}}
+          {{#if c.footer.website}}{{#if c.footer.company}}<a href="{{c.footer.website}}" target="_new">{{c.footer.company}}</a>{{/if}}{{/if}}
+          {{#if c.footer.version}} (v: {{c.footer.version}}){{/if}}
+          {{#if c.footer.email}}<br><a href="mailto:{{c.footer.email}}?subject={{c.footer.subject}}">Contact Admin</a>{{/if}}
         </div>
         <div class="col-md-4 hidden-xs text-muted credit" style="font-size:9px;text-align:center;">
           {{#if c.footer.title}}<strong style="font-size:11px;">{{c.footer.title}}</strong><br>{{/if}}
           {{c.footer.message}}
         </div>
         <div class="col-md-4 text-muted credit" style="font-size:10px;text-align:right;">
-          {{#if c.footer.year}}&copy; {{c.footer.year}}{{/if}}
-          {{#if c.footer.website}}{{#if c.footer.company}}<a href="{{c.footer.website}}" target="_new">{{c.footer.company}}</a>{{/if}}{{/if}}
-          {{#if c.footer.engineer.link}}{{#if c.footer.engineer.target}}{{#if c.footer.engineer.first_name}}{{#if c.footer.engineer.last_name}}<br>Engineered by <a href="{{c.footer.engineer.link}}" target="{{c.footer.engineer.target}}">{{c.footer.engineer.first_name}} {{c.footer.engineer.last_name}}</a>{{/if}}{{/if}}{{/if}}{{/if}}
+          {{#if c.footer.engineer.first_name}}{{#if c.footer.engineer.last_name}}Engineered by
+          {{#if c.footer.engineer.link}}
+            <a href="{{c.footer.engineer.link}}" target="{{c.footer.engineer.target}}">{{c.footer.engineer.first_name}} {{c.footer.engineer.last_name}}</a>
+          {{else}}
+            {{c.footer.engineer.first_name}} {{c.footer.engineer.last_name}}
+          {{/if}}
+          {{/if}}{{/if}}
+          {{#if c.footer.power.application}}<br>Powered by
+          {{#if c.footer.power.link}}
+            <a href="{{c.footer.power.link}}" target="{{c.footer.power.target}}">{{c.footer.power.application}}</a>
+          {{else}}
+            {{c.footer.power.application}}
+          {{/if}}
+          {{/if}}
         </div>
       </div>
     </div>

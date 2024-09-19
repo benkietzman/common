60d03e582c04398d077f7b48cd26ee0526737c29
Ben Kietzman <ben@kietzman.org>
2024-09-19 16:03:04 -0500

    Saved work.

--- www/js/component/Login.js
+++ www/js/component/Login.js
@@ -39,25 +39,42 @@ export default
     <br><br><br>
     {{#if c.login.showForm}}
     <div class="row">
-      <div class="col-md-auto">
+      <div class="col-md-4">
+      </div>
+      <div class="col-md-4">
         <div class="card">
-          <div class="card-header">
-            <h3 class="page-header">{{c.login.login.title}}</h3>
+          <div class="card-header bg-info fs-4 text-white">
+            <i class="bi bi-box-arrow-in-right"></i> {{c.login.login.title}}
           </div>
-          <div class="card-body row">
-            <div class="col-md-auto"><div class="input-group"><span class="input-group-text">User</span><input class="form-control" type="text" c-model="c.login.login.userid" maxlength="20" c-keyup="processLoginKey()" autofocus></div></div>
-            <div class="col-md-auto"><div class="input-group"><span class="input-group-text">Password</span><input class="form-control" type="password" c-model="c.login.login.password" maxlength="64" c-keyup="processLoginKey()"></div></div>
-            <div class="col-md-auto"><button class="btn btn-primary float-end" c-click="c.processLogin()">Login</button></div>
+          <div class="card-body">
+            <table class="table">
+            <tbody>
+            <tr><th class="fs-5">User</th><td><input class="form-control" type="text" c-model="c.login.login.userid" maxlength="20" c-keyup="processLoginKey()" autofocus></td</tr>
+            <tr><th class="fs-5">Password</th><td><input class="form-control" type="password" c-model="c.login.login.password" maxlength="64" c-keyup="processLoginKey()"></td></tr>
+            </tbody>
+            </table>
+          </div>
+          <div class="card-footer">
+            {{#if c.login.message}}
+            <b class="fs-5 text-danger">{{c.login.message}}</b>
+            {{/if}}
+            {{#if c.login.info}}
+            <span class="fs-5 text-warning">{{c.login.info}}</span>
+            {{/if}}
+            <button class="btn btn-success float-end" c-click="c.processLogin()">Login</button>
           </div>
         </div>
       </div>
+      <div class="col-md-4">
+      </div>
     </div>
-    {{/if}}
+    {{else}}
     {{#if c.login.message}}
     <div style="color:red;font-weight:bold;"><br>{{c.login.message}}</div>
     {{/if}}
     {{#if c.login.info}}
     <div style="color:orange;"><br>{{c.login.info}}</div>
+    {{/if}}
     {{/if}}
   `
   // }}}

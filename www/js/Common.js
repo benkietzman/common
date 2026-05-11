f8e687e06e1f5820e0b0a7b367bc858c9c1fd048
Ben Kietzman <ben@kietzman.org>
2026-05-11 14:28:39 -0500

    Saved work.

--- www/js/Common.js
+++ www/js/Common.js
@@ -2421,7 +2421,7 @@ class Common
   // {{{ setRedirectTimeout()
   setRedirectTimeout(unDuration)
   {
-    this.m_redirectTimeout = setTimeout(function() {document.location.href = c.getRedirectPath();}, unDuration);
+    this.m_redirectTimeout = setTimeout(function() {document.location.href = common.getRedirectPath();}, unDuration);
   }
   // }}}
   // {{{ setRequestPath()

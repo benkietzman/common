5712fa3440e1f3546199b0f08e50b52488222c2f
Ben Kietzman <ben@kietzman.org>
2026-05-12 10:00:16 -0500

    Saved work.

--- www/js/Common.js
+++ www/js/Common.js
@@ -2423,7 +2423,44 @@ class Common
   // {{{ setRerouteTimeout()
   setRerouteTimeout()
   {
-    this.m_rerouteTimeout = setTimeout(function() {document.location.href = common.m_strReroutePath;}, 5000);
+    this.m_unRerouteCount = 5;
+    let strMessage = 'Redirecting ';
+    if (this.m_unRerouteCount > 0)
+    {
+      strMessage += 'in ' + this.m_unRerouteCount + 'second';
+      if (this.m_unRerouteCount != 1)
+      {
+        strMessage += 's';
+      }
+    }
+    else
+    {
+      strMessage += 'now';
+    }
+    strMessage += '...';
+    document.getElementById("login_info").innerHtml(strMessage);
+    this.m_rerouteTimeout = setTimeout(function()
+    {
+      let strMessage = 'Redirecting ';
+      if (common.m_unRerouteCount > 0)
+      {
+        strMessage += 'in ' + common.m_unRerouteCount + 'second';
+        if (common.m_unRerouteCount != 1)
+        {
+          strMessage += 's';
+        }
+      }
+      else
+      {
+        strMessage += 'now';
+      }
+      strMessage += '...';
+      document.getElementById("login_info").innerHtml(strMessage);
+      if (common.m_unRerouteCount <= 0)
+      {
+        document.location.href = common.m_strReroutePath;
+      }
+    }, 1000);
   }
   // }}}
   // {{{ setRequestPath()
--- www/js/component/Login.js
+++ www/js/component/Login.js
@@ -110,7 +110,7 @@ export default
             <b class="fs-6 text-danger">{{c.login.message}}</b>
             {{/if}}
             {{#if c.login.info}}
-            <span class="fs-6 text-warning">{{c.login.info}}</span>
+            <span class="fs-6 text-warning" id="login_info">{{c.login.info}}</span>
             {{/if}}
             {{#if c.login.showForm}}
             <button class="btn btn-primary float-end" c-click="processLogin()">Login</button>

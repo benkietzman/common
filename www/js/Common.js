7806f1889a809c8a5eba40e6e120273d7b404e55
Ben Kietzman <ben@kietzman.org>
2026-08-12 09:49:11 -0500

    Saved work.

--- www/js/Common.js
+++ www/js/Common.js
@@ -2100,12 +2100,6 @@ class Common
     }
   }
   // }}}
-  // {{{ replaceAt()
-  replaceAt(strValue, nIndex, strReplacement, nRemove)
-  {
-    return strValue.slice(0, nIndex) + strReplacement + strValue.slice(nIndex + nRemove);
-  }
-  // }}}
   // {{{ request()
   request(strFunction, request, callback)
   {
